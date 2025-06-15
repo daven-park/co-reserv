5230cc21f0250c62404e21c51a77379deab3ead5
ChangHyun Park <buschanny@gmail.com>
2025-06-16 02:52:14 +0900

    Add : 로그인 요청 url 수정

--- front/src/components/User/Login.tsx
+++ front/src/components/User/Login.tsx
@@ -88,7 +88,7 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
+      const response = await fetch('http://localhost:8000/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
