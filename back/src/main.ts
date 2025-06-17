92835560cb11792ff2927f43719f71877e0ba67b
ChangHyun Park <buschanny@gmail.com>
2025-06-17 11:02:35 +0900

    Refactor : 테스트 유저 생성 함수 제거

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,31 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DataSource } from 'typeorm';
-import { User } from './users/user.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // 데이터베이스 연결
-  const dataSource = app.get(DataSource);
-
-  // 테스트 데이터 생성
-  const userRepository = dataSource.getRepository(User);
-  const existingUser = await userRepository.findOne({
-    where: { userId: 'test' },
-  });
-
-  if (!existingUser) {
-    const testUser = userRepository.create({
-      userId: 'test',
-      password: 'test123',
-      name: '테스트유저',
-    });
-    await userRepository.save(testUser);
-    console.log('테스트 유저 생성 완료');
-  } else {
-    console.log('테스트 유저가 이미 존재합니다.');
-  }
 
   app.enableCors({
     origin: 'http://localhost:3000',
